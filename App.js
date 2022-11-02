88e946cda5160976e932b1796c1e2ad75abec11c
Jonas Diete <jonas.diete@gmail.com>
2022-11-02 15:37:40 +0000

    can send notification at specific time/schedule

--- App.js
+++ App.js
@@ -3,19 +3,11 @@ import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Notifications from 'expo-notifications';
-// import { Expo } from 'expo-server-sdk';
-
-let pushToken;
-// let expo = new Expo();
 
 const registerForPushNotificationsAsync = async () => {
-  let token;
-
-  if (Constants.isDevice) {
     // we check if we have access to the notification permission
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
-
 
     if (existingStatus !== 'granted') {
       // if we dontt have access to it, we ask for it
@@ -28,16 +20,6 @@ const registerForPushNotificationsAsync = async () => {
       return;
     }
 
-    // obtain the expo token
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-
-    // log the expo token in order to play with it
-    console.log(token);
-  } else {
-
-    // notifications only work on physcal devices
-    alert('Must use physical device for Push Notifications');
-  }
   // some android configuration
   if (Platform.OS === 'android') {
     Notifications.setNotificationChannelAsync('default', {
@@ -48,8 +30,6 @@ const registerForPushNotificationsAsync = async () => {
     });
   }
 
-  pushToken = token;
-  return token;
 }
 
 export default function App() {
@@ -89,36 +69,6 @@ export default function App() {
     console.log('A notification has been touched', data)
   }
 
-  // // SENDING A PUSH NOTIFICATION
-  // let messages = [];
-  // messages.push({
-  //   to: pushToken,
-  //   sound: 'default',
-  //   body: 'This is a test notification',
-  //   data: { withSome: 'data' },
-  // })
-
-  // let chunks = expo.chunkPushNotifications(messages);
-  // let tickets = [];
-  // (async () => {
-  //   // Send the chunks to the Expo push notification service. There are
-  //   // different strategies you could use. A simple one is to send one chunk at a
-  //   // time, which nicely spreads the load out over time:
-  //   for (let chunk of chunks) {
-  //     try {
-  //       let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-  //       console.log(ticketChunk);
-  //       tickets.push(...ticketChunk);
-  //       // NOTE: If a ticket contains an error code in ticket.details.error, you
-  //       // must handle it appropriately. The error codes are listed in the Expo
-  //       // documentation:
-  //       // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  // })();
-
   return (
     <View style={styles.container}>
       <Text>Wakify - matches your mood to a playlist</Text>
@@ -126,31 +76,26 @@ export default function App() {
       <Button
         title="Press to Send Notification"
         onPress={async () => {
-          await sendPushNotification(pushToken);
+          await schedulePushNotification();
         }}
       />
     </View>
   );
 }
 
-async function sendPushNotification(expoPushToken) {
-  const message = {
-    to: expoPushToken,
-    sound: 'default',
-    title: 'Original Title',
-    body: 'And here is the body!',
-
-    data: { someData: 'goes here' },
-  };
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Good Morning!",
+      body: 'Tap to set your mood',
+      data: { data: 'goes here' },
     },
-    body: JSON.stringify(message),
+    trigger: { 
+      seconds: 10,
+      // hour: 8,
+      // minute: 0,
+      // repeats: true
+    },
   });
 }
 
