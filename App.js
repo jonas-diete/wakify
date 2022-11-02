e9379f32fb19578a9d833f3dc8048f2c0d849e70
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-11-02 15:01:54 +0000

    Added emojis to homepage

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Button, Text, View, Alert, TouchableOpacity, Image  } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Wakify - matches your mood to a playlist</Text>
       <StatusBar style="auto" />
+      <View>
+        <TouchableOpacity onPress={() => Alert.alert("Happy Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/happy.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Neutral Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/neutral.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Sad Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/sad.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Distraught Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/distraught.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Angry Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/angry.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Tired Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/tired.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("Active Emoji pressed")}>
+          <Image style={styles.button} source={require('./assets/active.png')} />
+        </TouchableOpacity>
+      </View>
     </View>
+    
   );
 }
 
@@ -17,4 +43,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 100 / 2,
+  }
 });
