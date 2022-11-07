e99b3ffc76e2eade1e19a563525ca745c2634dec
Liovirgilda Mendonca <liovirgildamendonca@MASTUDEMB03Q6L4.lan>
2022-11-07 15:16:13 +0000

    Tapped Notification goes to mood

--- screens/home.js
+++ screens/home.js
@@ -1,8 +1,36 @@
 import { Text, View, Button} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import styles from '../src/utils/styles.js'
+import registerForPushNotificationsAsync from '../src/utils/registerForPushNotifications';
+import React, { useEffect, useRef } from 'react';
+import * as Notifications from 'expo-notifications';
+
 
 function Home({ navigation }){
+  const responseListener = useRef();
+
+  useEffect( () => {
+    // Register device for push notifications
+    registerForPushNotificationsAsync();
+
+    // Listening for user tapping/interacting with notification
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      notificationNavigationHandler(response.notification.request.content);
+    });
+
+    // removing listeners
+    return () => {
+
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
+
+  const notificationNavigationHandler = ({ data }) => {
+    // add logic here to navigate to a specific app screen
+    navigation.navigate('SelectMood')
+  }
+
   return(
   <View style={styles.container}>
       <StatusBar style="auto" />
