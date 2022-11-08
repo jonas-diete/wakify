8eccd8025c7fc028745fc22145a901f58f171a3c
Jonas Diete <jonas.diete@gmail.com>
2022-11-08 17:01:44 +0000

    deleted details screen

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import * as Notifications from 'expo-notifications';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/home.js'
-import DetailsScreen from './screens/details.js'
 import SelectTime from './screens/SelectTime'
 import SelectMood from './screens/SelectMood'
 import SelectGenre from './screens/SelectGenre'
@@ -23,7 +22,6 @@ function App(){
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="SelectTime" component={SelectTime} />
         <Stack.Screen name="SelectMood" component={SelectMood} />
         <Stack.Screen name="SelectGenre" component={SelectGenre} />
