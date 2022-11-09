ae89804f0a52049b5c1dfc245d000b603619c1b2
Jonas Diete <83158738+jonas-diete@users.noreply.github.com>
2022-11-09 16:40:57 +0000

    Update user_can_select_mood.cy.js

--- cypress/e2e/user_can_select_mood.cy.js
+++ cypress/e2e/user_can_select_mood.cy.js
@@ -8,16 +8,17 @@ describe("SelectMood", () => {
   it("should click on Authorize button", () => {
     cy.visit("/");
     cy.contains("Mood").click();
+    cy.contains("Authorize Spotify").click();
 
-    //
-
+    // Cypress doesn't allow us to test this pop-up window, so this test cannot be completed.
+    
     // cy.window().then((win) => {
     //   cy.stub(win, "open", (url) => {
     //     win.location.href =
     //       "https://apresolve.spotify.com/?type=dealer&type=spclient";
     //   }).as("popup");
     // });
-    cy.contains("Authorize Spotify").click();
+    // cy.contains("Authorize Spotify").click();
     // cy.get("@popup").should("be.called");
     // cy.contains("Password").should("be.visible");
   });
