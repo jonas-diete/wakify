b689a6b6e2c13f2ba7c139efd25e9d9c47afbf84
Jonas Diete <jonas.diete@gmail.com>
2022-11-09 16:30:03 +0000

    time can be changed and saved

--- cypress/e2e/user_can_select_time.cy.js
+++ cypress/e2e/user_can_select_time.cy.js
@@ -11,5 +11,32 @@ describe('SelectTime', () => {
     cy.get('div').contains('08');
     cy.get('div').contains('00');
   })
+
+  it('user can select time and it displays', () => {
+    cy.visit('/');
+    cy.get('div').contains('Time').click();
+    cy.get('div').contains('08').click();
+    cy.get('div').contains('07').click();
+    cy.get('div').contains('00').click();
+    cy.get('div').contains('05').click();
+  })
+
+  it('user can select time and it displays', () => {
+    cy.visit('/');
+    cy.get('div').contains('Time').click();
+    cy.get('div').contains('Your notification time is not chosen yet.');
+  })
+
+  it('user can select time and it displays', () => {
+    cy.visit('/');
+    cy.get('div').contains('Time').click();
+    cy.get('div').contains('08').click();
+    cy.get('div').contains('07').click();
+    cy.get('div').contains('00').click();
+    cy.get('div').contains('05').click();
+    cy.get('div').contains('Submit').click();
+    cy.get('div').contains('Time').click();
+    cy.get('div').contains('Your notification time is 7:05');
+  })
 })
 
