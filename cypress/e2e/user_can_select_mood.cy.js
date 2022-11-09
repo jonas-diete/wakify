b48bd7b0947eeeab28a31e91ccc903e54a1fe012
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-11-09 15:24:57 +0000

    Added tests for select genre and mood

--- cypress/e2e/user_can_select_mood.cy.js
+++ cypress/e2e/user_can_select_mood.cy.js
@@ -1,7 +1,24 @@
-describe('SelectMood', () => {
-  it('should get to mood screen', () => {
-    cy.visit('/');
-    cy.contains('Mood').click();
-    cy.get('title').contains('Mood');
-  })
-})
+describe("SelectMood", () => {
+  it("should get to mood screen", () => {
+    cy.visit("/");
+    cy.contains("Mood").click();
+    cy.get("title").contains("Mood");
+  });
+
+  it("should click on Authorize button", () => {
+    cy.visit("/");
+    cy.contains("Mood").click();
+
+    //
+
+    // cy.window().then((win) => {
+    //   cy.stub(win, "open", (url) => {
+    //     win.location.href =
+    //       "https://apresolve.spotify.com/?type=dealer&type=spclient";
+    //   }).as("popup");
+    // });
+    cy.contains("Authorize Spotify").click();
+    // cy.get("@popup").should("be.called");
+    // cy.contains("Password").should("be.visible");
+  });
+});
