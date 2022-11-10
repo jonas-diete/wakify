3522d5414dc5f6fff202f3bfbdc4130e203e5472
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-11-10 16:43:29 +0000

    Web working

--- screens/home.js
+++ screens/home.js
@@ -1,59 +1,58 @@
-import { Text, View, Image} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import styles from '../src/utils/styles.js'
-import registerForPushNotificationsAsync from '../src/utils/registerForPushNotifications';
-import React, { useEffect, useRef } from 'react';
-import * as Notifications from 'expo-notifications';
-import Pressable from 'react-native/Libraries/Components/Pressable/Pressable.js';
+import { Text, View, Image, Pressable } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import styles from "../src/utils/styles.js";
+import registerForPushNotificationsAsync from "../src/utils/registerForPushNotifications";
+import React, { useEffect, useRef } from "react";
+import * as Notifications from "expo-notifications";
 
-
-function Home({ navigation }){
+function Home({ navigation }) {
   const responseListener = useRef();
 
-  useEffect( () => {
+  useEffect(() => {
     // Register device for push notifications
     registerForPushNotificationsAsync();
 
     // Listening for user tapping/interacting with notification
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      notificationNavigationHandler(response.notification.request.content);
-    });
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        notificationNavigationHandler(response.notification.request.content);
+      });
 
     // removing listeners
     return () => {
-
       Notifications.removeNotificationSubscription(responseListener);
     };
   }, []);
 
-
   const notificationNavigationHandler = ({ data }) => {
     // add logic here to navigate to a specific app screen
-    navigation.navigate('SelectMood')
-  }
+    navigation.navigate("SelectMood");
+  };
 
-  return(
-  <View style={styles.container}>
+  return (
+    <View style={styles.container}>
       <StatusBar style="auto" />
-      <Image
-        style={styles.logo}
-        source={require('../assets/wakify.png')}
-      />
-      <Pressable style={styles.button}
-         onPress={() => navigation.navigate('SelectTime')}>
+      <Image style={styles.logo} source={require("../assets/wakify.png")} />
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("SelectTime")}
+      >
         <Text style={styles.text}>Notification Time</Text>
       </Pressable>
-      <Pressable style={styles.button} 
-        onPress={() => navigation.navigate('SelectMood')}>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("SelectMood")}
+      >
         <Text style={styles.text}>Select Your Mood</Text>
       </Pressable>
-      <Pressable style={styles.button} 
-        onPress={() => navigation.navigate('SelectGenre')}>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("SelectGenre")}
+      >
         <Text style={styles.text}>Favourite Genres</Text>
       </Pressable>
     </View>
   );
 }
 
-  
 export default Home;
